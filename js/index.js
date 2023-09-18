494c6f13c58a0d50185c4597c7ae86b28aba269f
DaviDM2005 <87268065+DaviDM2005@users.noreply.github.com>
2023-09-18 23:48:24 +0400

    beta test

--- js/index.js
+++ js/index.js
@@ -1,36 +1,7 @@
-// Navbar
-
-const buttons = document.querySelectorAll('.nav-bar li a');
-
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const checkbox = document.getElementById('check');
-    checkbox.checked = false;
-  });
-});
 
 
 
-var inputs = document.querySelectorAll(".my-input");
 
-for (var i = 0; i < inputs.length; i++) {
-  var input = inputs[i];
-
- 
-  input.addEventListener("focus", function() {
-    this.style.backgroundColor = "black"; 
-  });
-
- 
-  input.addEventListener("blur", function() {
-    this.style.backgroundColor = ""; 
-  });
-
- 
-  input.addEventListener("input", function() {
-    this.style.backgroundColor = "black"; 
-  });
-}
 
 
 // collapsible menu

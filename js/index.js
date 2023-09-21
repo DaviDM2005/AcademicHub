441c72acbb2c9de63eda48d13f8bd9079c4427d8
DaviDM2005 <87268065+DaviDM2005@users.noreply.github.com>
2023-09-21 21:16:28 +0400

    beta test

--- js/index.js
+++ js/index.js
@@ -47,6 +47,22 @@ function search() {
 
 
 
+// reveal animation
+
+const filterDivs = document.querySelectorAll('.filterDiv');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = '1';
+            entry.target.style.transform = 'translateY(0)';
+        }
+    });
+});
+
+filterDivs.forEach(div => {
+    observer.observe(div);
+});
 
 
 

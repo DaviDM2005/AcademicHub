0e56fa5f65769a92f87b60e7bd79a0e082a4e4b9
DaviDM2005 <87268065+DaviDM2005@users.noreply.github.com>
2023-09-23 00:32:12 +0400

    project 2.0

--- js/index.js
+++ js/index.js
@@ -7,24 +7,45 @@
 // collapsible menu
 
 var coll = document.getElementsByClassName("collapsible");
-        var i;
-        
-        for (i = 0; i < coll.length; i++) {
-          coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight){
-              content.style.maxHeight = null;
-            } else {
-              content.style.maxHeight = content.scrollHeight + "px";
-            } 
-          });
-        }
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function(event) {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+    
+    
+    event.stopPropagation();
+  });
+}
+
+
+document.addEventListener("click", function(event) {
+  var coll = document.getElementsByClassName("collapsible");
+  for (var i = 0; i < coll.length; i++) {
+    var content = coll[i].nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+      coll[i].classList.remove("active");
+    }
+  }
+});
+
+
+document.querySelector(".filterbtndiv").addEventListener("click", function(event) {
+  event.stopPropagation();
+});
 
 
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
 
 
 // search system
